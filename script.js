d83c86bf9b59af115a88a2defed3f8f7f13b50c9
Danial <menunem6@gmail.com>
2024-12-09 10:59:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const Eshop = {
+    names:'E-Shop',phoneNumber:87715669330,
+    phoneNumber:87715669330,
+    products: [
+        {productName:'samsa', category:['ұн ет пияз'], price: 400, stock:10 },
+        {productName:'Cola', category:['Қант Су Сусын'], price: 530, stock:50 },
+        {productName:'orbit', category:['дәмдеуіш қант қант'], price:290, stock:25 },
+        {productName:'alma', category:['Таабиғи өнім'], price: 200, stock:75 },
+        {productName:'nan', category:['ұн жұмыртқа тұз'], price: 120, stock:8 }
+    ]
+}
+
+// function changeShopName ()  {
+//  let aty = prompt('Duken atayu')
+//  return Eshop.names = aty
+// }
+// alert(changeShopName())
+
+// function changePhoneNumber () {
+//     let num = +prompt('nomer qalai')
+//     return Eshop.phoneNumber = num
+// }
+// alert(changePhoneNumber())
+
+// function listProductNames(){
+    
+// }
+// alert(listProductNames())
+
+// function addProduct () {
+//     let pro = prompt('aty')
+//     let pri = +prompt('bagasy')
+//     let pra = +prompt('qansha')
+//     let pry = prompt('categoria')
+//    return Eshop.products.push =+ (pro, pri,pra,pry)
+// }
+// console.log(addProduct());
+
+// function applyDiscountToCategory () {
+//     let a = prompt('Categoria')
+//     let b = +prompt('Skidka')
+//     Eshop.products * 0.1
+// }
+// applyDiscountToCategory()
+
+// function menu () {
+//     while(true){
+//         'Шығу'
+//         break
+//     }
+//     let  = +prompt('1 den 6 san tandanyz')
+//     if(1) {
+//           changeShopName()
+//     } else if(2) {
+//           changePhoneNumber()
+//     } else if(3){
+//           listProductNames()
+//     } else if(4){
+//          addProduct()
+//     } else if (5){
+//          applyDiscountToCategory()
+//     } else if(6){
+//         alert('Sau bol')
+//     }
+// }
+// alert(menu())
+    
+
